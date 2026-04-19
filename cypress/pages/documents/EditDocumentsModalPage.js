d89214c215c2d792dafcffabeaf91a4703399013
Rakshita Baidya <119068178+Rakshita-Baidya@users.noreply.github.com>
2026-04-20 00:41:14 +0545

    refactor: extend AddDocumentsModalPage and EditDocumentsModalPage from BaseTablePage, remove unused methods

--- cypress/pages/documents/EditDocumentsModalPage.js
+++ cypress/pages/documents/EditDocumentsModalPage.js
@@ -1,54 +1,24 @@
-class EditDocumentsModalPage {
-  get modalTitle() {
-    return cy.contains(
-      "div.bg-sidebar-primary",
-      /Edit Document|Update Document/,
-    );
-  }
+const BaseTablePage = require("../common/BaseTablePage");
 
+class EditDocumentsModalPage extends BaseTablePage {
+  get modalTitle() {
+    return cy.contains("div.bg-sidebar-primary", /Edit Document/);
+  }
   get nameInput() {
     return cy.get('input[placeholder*="Enter Name"], input[name="name"]');
   }
-
   get countriesSelect() {
     return cy.get('button[role="combobox"]').filter(":visible");
   }
-
   get updateBtn() {
     return cy.contains("button", "Update Document");
   }
-
-  updateField(input, newValue) {
-    if (!newValue) return;
-
-    input.then(($el) => {
-      const currentValue = $el.val?.() ?? $el.text?.();
-      if (currentValue !== newValue) {
-        cy.wrap($el).clear().type(String(newValue));
-      }
-    });
-  }
-  selectCountries(countries) {
-    if (!countries || !countries.length) return;
-
-    this.countriesSelect.click();
-
-    countries.forEach((country) => {
-      cy.document().then((doc) => {
-        cy.wrap(doc.body)
-          .find('[role="option"]', { timeout: 10000 })
-          .contains(country)
-          .then(($el) => {
-            $el[0].scrollIntoView({ block: "center" });
-            $el[0].click();
-          });
-      });
-    });
+  get cancelBtn() {
+    return cy.contains("button", "Cancel");
   }
 
   fillForm(documentData) {
     if (documentData.name) this.updateField(this.nameInput, documentData.name);
-
     if (documentData.countries) this.selectCountries(documentData.countries);
   }
 
